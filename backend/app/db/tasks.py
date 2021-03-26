a688248c8482d9f457535c0106be3089785df381
Sten Remmelg <sten.remmelg@gmail.com>
2021-03-26 23:17:36 +0100

    conncet to test db then testing env variable is set

--- backend/app/db/tasks.py
+++ backend/app/db/tasks.py
@@ -9,9 +9,8 @@ logger = logging.getLogger(__name__)
 
 
 async def connect_to_db(app: FastAPI) -> None:
-    database = Database(
-        DATABASE_URL, min_size=2, max_size=10
-    )  # these can be configured in config as well
+    DB_URL = f"{DATABASE_URL}_test" if os.environ.get("TESTING") else DATABASE_URL
+    database = Database(DB_URL, min_size=2, max_size=10)
     try:
         await database.connect()
         app.state._db = database
