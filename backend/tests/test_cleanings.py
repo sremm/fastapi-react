4e6f04e50aeb3ad2be9f090976f3c6c419ccd49c
Sten Remmelg <sten.remmelg@gmail.com>
2021-03-26 23:44:06 +0100

    add a happy and unhappy path test for create cleaning

--- backend/tests/test_cleanings.py
+++ backend/tests/test_cleanings.py
@@ -1,18 +1,67 @@
 import pytest
+from app.models.cleaning import CleaningCreate
 from fastapi import FastAPI
 from httpx import AsyncClient
-from starlette.status import HTTP_404_NOT_FOUND, HTTP_422_UNPROCESSABLE_ENTITY
+from starlette.status import (
+    HTTP_201_CREATED,
+    HTTP_404_NOT_FOUND,
+    HTTP_422_UNPROCESSABLE_ENTITY,
+)
+
+# decorate all tests with @pytest.mark.asyncio
+pytestmark = pytest.mark.asyncio
+
+
+@pytest.fixture
+def new_cleaning():
+    return CleaningCreate(
+        name="test cleaning",
+        description="test description",
+        price=0.00,
+        cleaning_type="spot_clean",
+    )
 
 
 class TestCleaningsRoutes:
-    @pytest.mark.asyncio
     async def test_routes_exist(self, app: FastAPI, client: AsyncClient) -> None:
         res = await client.post(app.url_path_for("cleanings:create-cleaning"), json={})
         assert res.status_code != HTTP_404_NOT_FOUND
 
-    @pytest.mark.asyncio
     async def test_invalid_input_raises_error(
         self, app: FastAPI, client: AsyncClient
     ) -> None:
         res = await client.post(app.url_path_for("cleanings:create-cleaning"), json={})
         assert res.status_code == HTTP_422_UNPROCESSABLE_ENTITY
+
+
+class TestCreateCleaning:
+    async def test_valid_input_creates_cleaning(
+        self, app: FastAPI, client: AsyncClient, new_cleaning: CleaningCreate
+    ) -> None:
+        res = await client.post(
+            app.url_path_for("cleanings:create-cleaning"),
+            json={"new_cleaning": new_cleaning.dict()},
+        )
+        assert res.status_code == HTTP_201_CREATED
+
+        created_cleaning = CleaningCreate(**res.json())
+        assert created_cleaning == new_cleaning
+
+    @pytest.mark.parametrize(
+        "invalid_payload, status_code",
+        (
+            (None, 422),
+            ({}, 422),
+            ({"name": "test_name"}, 422),
+            ({"price": 10.00}, 422),
+            ({"name": "test_name", "description": "test"}, 422),
+        ),
+    )
+    async def test_invalid_input_raises_error(
+        self, app: FastAPI, client: AsyncClient, invalid_payload: dict, status_code: int
+    ) -> None:
+        res = await client.post(
+            app.url_path_for("cleanings:create-cleaning"),
+            json={"new_cleaning": invalid_payload},
+        )
+        assert res.status_code == status_code
