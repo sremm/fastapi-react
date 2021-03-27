4be75bbcb089dc2fa5ee68b496f82685b8b19fc4
Sten Remmelg <sten.remmelg@gmail.com>
2021-03-27 21:20:09 +0100

    add endpoint fr delete cleaning

--- backend/app/api/routes/cleanings.py
+++ backend/app/api/routes/cleanings.py
@@ -2,12 +2,8 @@ from typing import List
 
 from app.api.dependencies.database import get_repository
 from app.db.repositories.cleanings import CleaningsRepository
-from app.models.cleaning import (
-    CleaningCreate,
-    CleaningInDB,
-    CleaningPublic,
-    CleaningUpdate,
-)
+from app.models.cleaning import (CleaningCreate, CleaningInDB, CleaningPublic,
+                                 CleaningUpdate)
 from fastapi import APIRouter, Body, Depends, HTTPException, Path
 from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
@@ -74,3 +70,13 @@ async def update_cleaning_by_id(
             status_code=HTTP_404_NOT_FOUND, detail="No cleaning found with that id."
         )
     return updated_cleaning
+
+@router.delete("/{id}/", response_model=int, name="cleanings:delete-cleaning-by-id")
+async def delete_cleaning_by_id(
+    id: int = Path(..., ge=1, title="The ID of the cleaning to delete."),
+    cleanings_repo: CleaningsRepository = Depends(get_repository(CleaningsRepository)),
+) -> int:
+    deleted_id = await cleanings_repo.delete_cleaning_by_id(id=id)
+    if not deleted_id:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No cleaning found with that id.")
+    return deleted_id
