46a06f9b4ad1b7abfd2b15efff114d6fc05c3f14
Sten Remmelg <sten.remmelg@gmail.com>
2021-03-28 13:56:50 +0200

    add authentication service

---
+++ backend/app/services/authentication.py
@@ -0,0 +1,28 @@
+import bcrypt
+from app.models.user import UserPasswordUpdate
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class AuthException(BaseException):
+    """
+    Custom auth exception that can be modified later on.
+    """
+
+    pass
+
+
+class AuthService:
+    def create_salt_and_hashed_password(
+        self, *, plaintext_password: str
+    ) -> UserPasswordUpdate:
+        salt = self.generate_salt()
+        hashed_password = self.hash_password(password=plaintext_password, salt=salt)
+        return UserPasswordUpdate(salt=salt, password=hashed_password)
+
+    def generate_salt(self) -> str:
+        return bcrypt.gensalt().decode()
+
+    def hash_password(self, *, password: str, salt: str) -> str:
+        return pwd_context.hash(password + salt)
